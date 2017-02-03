c139b19f3979c076c23761749ea00dad934d3b17
Robert Freedy <Robert Freedy>
2017-02-03 14:25:54 -0500

    Initial commit

---
+++ app/src/main/java/com/cse40333/rfreedy/lab2_rfreedy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cse40333.rfreedy.lab2_rfreedy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/cse40333/rfreedy/lab2_rfreedy/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.cse40333.rfreedy.lab2_rfreedy;
+
+/**
+ * Created by rfreedy on 2/3/2017.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
